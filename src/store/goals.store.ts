45088a327881407c206874ad1e27ded3e211df66
santidevhmo <santiswiftios@gmail.com>
2026-06-14 17:53:14 -0700

    feat: add toggleGoal action to goals store

--- src/store/goals.store.ts
+++ src/store/goals.store.ts
@@ -5,6 +5,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 interface GoalsState {
   selectedGoals: string[];
   setGoals: (goals: string[]) => void;
+  toggleGoal: (goal: string) => void;
 }
 
 /** Stores the user's selected nutrition goals in AsyncStorage. */
@@ -13,6 +14,12 @@ export const useGoalsStore = create<GoalsState>()(
     (set) => ({
       selectedGoals: [],
       setGoals: (goals) => set({ selectedGoals: goals }),
+      toggleGoal: (goal) =>
+        set((state) => ({
+          selectedGoals: state.selectedGoals.includes(goal)
+            ? state.selectedGoals.filter((g) => g !== goal)
+            : [...state.selectedGoals, goal],
+        })),
     }),
     {
       name: "goals-storage",
